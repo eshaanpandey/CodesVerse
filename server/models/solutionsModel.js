4a16f22e555935642779da6f93540a8cdbf49a6d
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2023-07-01 14:27:29 +0530

    setup

--- server/models/solutionsModel.js
+++ server/models/solutionsModel.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const solutionSchema = mongoose.Schema({
     problem: {type: mongoose.Types.ObjectId, ref: "problems"},
-    verdict: {type: String, enum: ["Passed", "Failed"]},
-    
+    verdict: {type: String, enum: ["Accepted", "Failed"]},
+    author: {type: mongoose.Types.ObjectId, ref: "user"},
+    submittedOn: Date,
 })
