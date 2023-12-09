dc02867580b0e8ef6b40e848d5edc800b1da1aad
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2023-12-09 22:45:02 +0530

    constraint area

--- client/src/components/Problems/ConstraintsArea.jsx
+++ client/src/components/Problems/ConstraintsArea.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function ConstraintsArea({ constraints }) {
   return (
-    <div className="mt-6">
-      <h1 className="mb-2">Constraints : </h1>
+    <div className="mt-6 font-serif">
+      <h1 className="mb-2">Constraints: </h1>
       {constraints &&
         constraints.map((constraint) => {
           return <p>{constraint}</p>;
