d7f9e6f036df796f754ce5a3ce584fffe5a7d74b
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2023-12-26 10:02:55 +0530

    redux problems

---
+++ client/src/redux/reducers/problems/problemsActions.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+import { GET_PROBLEMS_LIST, GET_PROBLEM_BY_ID } from "./problemsTypes";
+
+export const getProblemsList = () => async (dispatch) => {
+  try {
+    const problems = await axios({
+      url: "http://localhost:4000/CodeSlayer/problems/",
+      method: "GET",
+    });
+
+    return dispatch({ type: GET_PROBLEMS_LIST, payload: problems });
+  } catch (error) {
+    return dispatch({ type: "ERROR", payload: error });
+  }
+};
+
+export const getProblemById = (_id) => async (dispatch) => {
+  try {
+    const problem = await axios({
+      url: `http://localhost:4000/CodeSlayer/problems/${_id}`,
+      method: "GET",
+    });
+
+    return dispatch({ type: GET_PROBLEM_BY_ID, payload: problem });
+  } catch (error) {
+    return dispatch({ type: "ERROR", payload: error });
+  }
+};
