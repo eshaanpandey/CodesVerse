8d671e123b48babf344c3cb83e8e9c733c69f610
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2024-01-05 20:58:43 +0530

    server

--- server/server.js
+++ server/server.js
@@ -18,8 +18,27 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "judge",
+  })
+);
 
-app.use('/judge', router)
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.use('/judge', router);
 
 app.listen(process.env.PORT, () => {
   db()
