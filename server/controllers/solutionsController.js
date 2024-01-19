c88f1b14e462af68c8dd60ca7cb19e27e65feb00
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2024-01-20 00:48:07 +0530

    compiler

--- server/controllers/solutionsController.js
+++ server/controllers/solutionsController.js
@@ -21,7 +21,107 @@ const getSolution = async(req, res) =>{
         return res.status(500).json({error: error.message});
     }
 }
+
+const runAProblem = async (req, res) => {
+    try {
+      const { language = "c++", code } = req.body;
+      const { problemId } = req.params;
+      const userId = req.session.passport.user._doc._id;
+  
+      const testcases = await TestcasesModel.find({
+        problem: problemId,
+        isExample: true,
+      });
+  
+      if (!code) {
+        return res.status(404).json({ success: false, error: "Empty code !!" });
+      }
+  
+      const filePath = await generateFile(language, code);
+      for (let i = 0; i < testcases.length; i++) {
+        const testcase = testcases[i];
+        const output = await executeCpp(filePath, testcase.input);
+        const pureStringOutout = output.replace(/(?:\r\n|\r|\n)/g, "");
+        const pureStringtestcaseOutput = testcase.output.replace(
+          /(?:\r\n|\r|\n)/g,
+          ""
+        );
+        const success =
+          pureStringOutout.toLowerCase() ==
+          pureStringtestcaseOutput.toLowerCase();
+  
+        if (!success) {
+          return res.status(200).json({
+            verdict: "Fail",
+            message: `Wrong Answer on testcase ${i + 1}`,
+          });
+        }
+      }
+  
+      return res
+        .status(200)
+        .json({ verdict: "Pass", message: `All Example testcases Passed` });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: error });
+    }
+  };
+  
+  const submitAProblem = async (req, res) => {
+    try {
+      const { language = "c++", code } = req.body;
+      const { problemId } = req.params;
+      const userId = req.session.passport.user._doc._id;
+  
+      const testcases = await TestcasesModel.find({
+        problem: problemId,
+      });
+  
+      if (!code) {
+        return res.status(404).json({ success: false, error: "Empty code !!" });
+      }
+  
+      for (let i = 0; i < testcases.length; i++) {
+        const testcase = testcases[i];
+        const filePath = await generateFile(language, code);
+        const output = await executeCpp(filePath, testcase.input);
+        const pureStringOutout = output.replace(/(?:\r\n|\r|\n)/g, "");
+        const pureStringtestcaseOutput = testcase.output.replace(
+          /(?:\r\n|\r|\n)/g,
+          ""
+        );
+        const success =
+          pureStringOutout.toLowerCase() ==
+          pureStringtestcaseOutput.toLowerCase();
+  
+        if (!success) {
+          const solution = await SolutionsModel.create({
+            problem: problemId,
+            verdict: "Fail",
+            message: `Wrong Answer on testcase ${i`` + 1}`,
+            submittedBy: userId,
+            submittedAt: new Date(),
+          });
+          return res.status(200).json({ solution });
+        }
+      }
+      const solution = await SolutionsModel.create({
+        problem: problemId,
+        verdict: "Pass",
+        message: `Accepted`,
+        submittedBy: userId,
+        submittedAt: new Date(),
+      });
+  
+      return res.status(200).json({ solution });
+    } catch (error) {
+      return res.status(500).json({ error: error });
+    }
+  };  
+
 export{
     addSolution,
     getSolution,
+    runAProblem,
+    submitAProblem
 };
---
+++ server/helper/codes/6efbe202-4297-401c-b988-1e4b584374b7.c++
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int n;
+  cin>>n;
+  cout<<n;
+  cout<<"Hello World<<endl;
+  cout<<n+5<<endl;
+}
