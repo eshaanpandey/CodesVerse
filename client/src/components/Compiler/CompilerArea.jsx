1a42d1d84b1a2f849aa7e3488a4b28843e2365e6
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2025-04-28 20:03:54 +0530

    update solution message

--- client/src/components/Compiler/CompilerArea.jsx
+++ client/src/components/Compiler/CompilerArea.jsx
@@ -23,9 +23,7 @@ function CompilerArea({ problemId }) {
 
   const dispatch = useDispatch();
   const { width, height } = useWindowSize();
-  const isLoggedIn = useSelector(
-    (state) => state.authReducer.isAuthenticated
-  );
+  const isLoggedIn = useSelector((state) => state.authReducer.isAuthenticated);
   const navigate = useNavigate();
 
   function closeModal() {
@@ -97,10 +95,14 @@ function CompilerArea({ problemId }) {
             error?.stderr?.split("error:")[1] || error || "Unknown error",
         });
       } else {
+        console.log("Payload in solutions", data.payload.data);
         const verdict =
           data.payload.data.solution.verdict === "Pass" ? "Pass" : "Fail";
         const message =
-          data.payload.data.solution.message || "Solution failed!";
+          // data.payload.data.solution.message || "Solution failed!";
+          data.payload.data.solution.verdict === "Pass"
+            ? "Solution passed successfully!"
+            : "Solution failed!";
         setSolution({
           verdict,
           message,
@@ -242,7 +244,7 @@ function CompilerArea({ problemId }) {
               onPress={onRun}
             />
             <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100">
-              {navigator.platform.includes("Mac") ? "Cmd+'" : "Ctrl+'" }
+              {navigator.platform.includes("Mac") ? "Cmd+'" : "Ctrl+'"}
             </div>
           </div>
           <div className="relative group">
