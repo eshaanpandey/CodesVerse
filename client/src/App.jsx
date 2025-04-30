c2d80dac24f2d50cd392fff3bb723ca2a9d22506
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2025-04-30 15:28:01 +0530

    remove unwanted logs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-// import { redirect } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import HomeLayout from "./layouts/HomeLayout.jsx";
@@ -9,7 +8,6 @@ import SubmitProblemScreen from "./screens/SubmitProblemsScreen.jsx";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getLoggedInUser } from "./redux/reducers/user/userActions.js";
-
 
 function App() {
   const dispatch = useDispatch();
@@ -22,17 +20,19 @@ function App() {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/signup" element={<LoginScreen />} />
-      {<Route
-        path="/:type"
-        element={
-          <HomeLayout>
-            <HomeScreen />
-          </HomeLayout>
-        }
-      /> }
+      {
+        <Route
+          path="/:type"
+          element={
+            <HomeLayout>
+              <HomeScreen />
+            </HomeLayout>
+          }
+        />
+      }
       <Route path="/problems/:_id" element={<SubmitProblemScreen />} />
     </Routes>
-);
+  );
 }
 
 export default App;
--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -2,21 +2,15 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Home from "../components/Home/Home";
 import Problems from "../components/Problems/Problems";
-import Contests from "../components/Contests/Contests";
 import Profile from "../components/Profile/Profile";
 
 function HomeScreen() {
-    const { type } = useParams();
-    let Component = <Home />;
-    if (type === "problems") Component = <Problems />;
-    else if (type === "contests") Component = <Contests />;
-    else if (type === "profile") Component = <Profile />;
+  const { type } = useParams();
+  let Component = <Home />;
+  if (type === "problems") Component = <Problems />;
+  else if (type === "profile") Component = <Profile />;
 
-    return (
-        <div className="w-full h-3/4">
-            {Component}
-        </div>
-    );
+  return <div className="w-full h-3/4">{Component}</div>;
 }
 
 export default HomeScreen;
